839471b8039e563fdf75824f812062963bc167a5
Kevin Peters <git.igeligel@googlemail.com>
2017-04-15 21:24:57 +0200

    feat(openid)
    implement first models of open id parameters

---
+++ TeamFortressOutpostApi/Interfaces/IOpenIdParameters.cs
@@ -0,0 +1,10 @@
+﻿namespace HedgehogSoft.TeamFortressOutpostApi.Interfaces
+{
+    public interface IOpenIdParameters
+    {
+        string Action { get; set; }
+        string OpenIdMode { get; set; }
+        string OpenIdParams { get; set; }
+        string Nonce { get; set; }
+    }
+}
---
+++ TeamFortressOutpostApi/Models/OpenIdParameters.cs
@@ -0,0 +1,12 @@
+﻿using HedgehogSoft.TeamFortressOutpostApi.Interfaces;
+
+namespace HedgehogSoft.TeamFortressOutpostApi.Models
+{
+    internal class OpenIdParameters : IOpenIdParameters
+    {
+        public string Action { get; set; }
+        public string OpenIdMode { get; set; }
+        public string OpenIdParams { get; set; }
+        public string Nonce { get; set; }
+    }
+}
