c2aaa926f01fb05b771ede5017520e3730b99c94
Kevin Peters <git.igeligel@googlemail.com>
2017-04-17 20:24:25 +0200

    feat(project)
    add example project

---
+++ examples/TeamFortressOutpostApiConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using HedgehogSoft.TeamFortressOutpostApi;
+
+namespace TeamFortressOutpostApiConsole
+{
+    internal class Program
+    {
+        // ReSharper disable once UnusedMember.Local
+        private static void Main()
+        {
+            Loader.LoadSettings();
+            var teamFortressOutpostApiClient = new TeamFortressOutpostApiClient();
+            Console.ReadKey();
+            teamFortressOutpostApiClient.Login(
+                ConsoleSettings.Instance["username"], 
+                ConsoleSettings.Instance["password"], 
+                ConsoleSettings.Instance["sharedSecret"]);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ examples/TeamFortressOutpostApiConsole/Settings.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace TeamFortressOutpostApiConsole
+{
+    internal static class ConsoleSettings
+    {
+        internal static Dictionary<string, string> Instance = new Dictionary<string, string>();
+    }
+}
